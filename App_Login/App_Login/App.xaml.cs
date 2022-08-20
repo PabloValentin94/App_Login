b4e943cbc1ab2b6649c43e9def12d9e0f84a4ad6
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-08-20 20:07:58 -0300

    Atualizando Projeto.

--- App_Login/App_Login/App.xaml.cs
+++ App_Login/App_Login/App.xaml.cs
@@ -2,11 +2,31 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using System.Collections.Generic;
+
+using App_Login.View;
+using App_Login.View.Area_Restrita;
+using App_Login.Model;
+
 namespace App_Login
 {
 
     public partial class App : Application
     {
+
+        public List<Dados_Usuario> lista_usuarios = new List<Dados_Usuario>
+        {
+
+            new Dados_Usuario()
+            {
+
+                usuario = "Pablo Valentin",
+
+                senha = "211066"
+
+            }
+
+        };
 
         public App()
         {
@@ -18,14 +38,14 @@ namespace App_Login
 
                 string usuario = Properties.ContainsKey("PersistenciaUsuarioLogado").ToString();
 
-                MainPage = new NavigationPage(new View.Area_Restrita.Area_Restrita());
+                MainPage = new NavigationPage(new Area_Restrita());
 
             }
 
             else
             {
 
-                MainPage = new NavigationPage(new View.Login());
+                MainPage = new NavigationPage(new Login());
 
             }
 
---
+++ App_Login/App_Login/Model/Dados_Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App_Login.Model
+{
+
+    public class Dados_Usuario
+    {
+
+        public string usuario {get; set;}
+
+        public string senha {get; set;}
+
+    }
+
+}
